b3046d648b16c893811fa95410d29936f31bfdf9
Alexander F. Rødseth <rodseth@gmail.com>
2019-06-24 14:21:35 +0200

    Add code for converting numbers to XPM palette index words

--- xpm.go
+++ xpm.go
@@ -5,17 +5,11 @@ import (
 	"image"
 	"image/color"
 	"io"
-	"strings"
+	//	"strings"
 )
 
 // Encoder contains encoding configuration that is used by the Encode method
 type Encoder struct {
-	// The internal image name, when encoding the data. The default is "img".
-	Name string
-
-	// The threshold, from 0 to 1, for when grayscale colors should appear as
-	// white or black. The default is 0.5.
-	Threshold float64
 }
 
 // hexify converts a slice of bytes to a slice of hex strings on the form 0x00
@@ -31,59 +25,108 @@ func hexify(data []byte) (r []string) {
 	return r
 }
 
+func c2hex(c color.Color) string {
+	return "#2080ff"
+}
+
+// lastIs checks if the last n letters of the given string is the given letter
+func lastIs(s string, n int, letter byte) bool {
+	if n >= len(s) {
+		panic("n is too large")
+	}
+	for i := n; i >= len(s)-n; i-- {
+		if s[i] != letter {
+			return false
+		}
+	}
+	return true
+}
+
+// inc will advance to the next string. Uses a-z. From "a" to "b", "zz" to "aaa" etc
+func inc(s string) string {
+	if s == "" {
+		return "a"
+	}
+	if len(s) == 1 {
+		if s[len(s)-1] != 'z' { // one digit, not z
+			return string(s[0] + 1) // return the next letter
+		}
+		// one digit, and it is 'z'
+		return "aa"
+	}
+	if s[len(s)-1] == 'z' { // two or more digits, the last digit is z
+		return inc(s[:len(s)-1]) + "a" // increase next to last digit with one + "a"
+	}
+	// two or more digits, the last digit is not z
+	return s[:len(s)-1] + inc(string(s[len(s)-1])) // first digit + last digit increases with one
+}
+
+// num2charcode converts a number to ascii letters, like 0 to "a", and 1 to "b".
+// Can output multiple letters for higher numbers.
+func num2charcode(num int) string {
+	// This is not the efficient way, but it's only called once per image conversion
+	d := "a"
+	for i := 0; i < num; i++ {
+		d = inc(d)
+	}
+	return d
+}
+
 // Encode will encode the given image as XBM, using a custom image name from
 // the Encoder struct. The colors are first converted to grayscale, and then
 // with a 50% cutoff they are converted to 1-bit colors.
 func (enc *Encoder) Encode(w io.Writer, m image.Image) error {
-    panic("not implemented yet")
-
-	imageName := enc.Name
-
 	width := m.Bounds().Dx()
 	height := m.Bounds().Dy()
 
-	fmt.Fprintf(w, "/* XBM X11 format */\n")
-
-	maskIndex := 0
-	masks := []uint8{
-		0x1,
-		0x2,
-		0x4,
-		0x8,
-		0x10,
-		0x20,
-		0x40,
-		0x80,
-	}
-
-	var pixels []byte
-	var pixel uint8
+	paletteMap := make(map[string]color.Color) // hexstring -> color, unordered
 	for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
 		for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
 			c := m.At(x, y)
-			grayColor := color.GrayModel.Convert(c).(color.Gray)
-			value := grayColor.Y
-			if value <= byte(float64(256)*enc.Threshold) {
-				// white
-				pixel |= masks[maskIndex]
-			} else {
-				// black, skip
-			}
-			// Prepare to write the next bit
-			maskIndex++
-			if maskIndex == len(masks) {
-				// Filled up an entire byte with pixel bits, flush and reset
-				maskIndex = 0
-				pixels = append(pixels, pixel)
-				pixel = 0
-			}
+			paletteMap[c2hex(c)] = c
 		}
 	}
 
-	fmt.Fprintf(w, "#define %s_width %d\n", imageName, width)
-	fmt.Fprintf(w, "#define %s_height %d\n", imageName, height)
-	fmt.Fprintf(w, "static unsigned char %s_bits[] = {\n", imageName)
-	fmt.Fprintf(w, "  %s\n", strings.Join(hexify(pixels), ", "))
+	var paletteSlice []string // hexstrings, ordered
+	for hexColor := range paletteMap {
+		paletteSlice = append(paletteSlice, hexColor)
+	}
+
+	// Find the character code of the highest index
+	highestCharCode := num2charcode(len(paletteSlice) - 1)
+	charsPerPixel := len(highestCharCode)
+	colors := len(paletteSlice)
+
+	// Write the header, now that we know the right values
+	fmt.Fprint(w, "/* XPM */\n")
+	fmt.Fprint(w, "static char * XFACE[] = {\n")
+	fmt.Fprint(w, "/* <Values> */\n")
+	fmt.Fprintf(w, "\"%d %d %d %d\",\n", width, height, colors, charsPerPixel)
+
+	// Write the colors of paletteSlice, and generate a lookup table from hexColor to charcode
+	fmt.Fprint(w, "/* <Colors> */\n")
+	lookup := make(map[string]string) // hexcolor -> paletteindexchars, unordered
+	charcode := ""
+	for index, hexColor := range paletteSlice {
+		charcode := inc(charcode)
+		fmt.Fprintf(w, "\"%s c %s\",\n", charcode, hexColor)
+		lookup[hexColor] = charcode
+		index++
+	}
+
+	// Now write the pixels, as character codes
+	fmt.Fprint(w, "/* <Pixels> */\n")
+	for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
+		fmt.Fprintf(w, "\"")
+		for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
+			c := m.At(x, y)
+			charcode := lookup[c2hex(c)]
+			fmt.Fprint(w, charcode)
+			// Now write c2hex(c) to the file
+		}
+		fmt.Fprintf(w, "\",\n")
+	}
+
 	fmt.Fprintf(w, "};\n")
 
 	return nil
@@ -91,6 +134,6 @@ func (enc *Encoder) Encode(w io.Writer, m image.Image) error {
 
 // Encode will encode the image as XBM, using "img" as the image name
 func Encode(w io.Writer, m image.Image) error {
-	e := &Encoder{"img", 0.5}
+	e := &Encoder{}
 	return e.Encode(w, m)
 }
---
+++ xpm_test.go
@@ -0,0 +1,42 @@
+package xpm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNum2charcode(t *testing.T) {
+	if num2charcode(0) != "a" {
+		t.Errorf("wanted a, got %s", num2charcode(0))
+	}
+	if num2charcode(1) != "b" {
+		t.Errorf("wanted b, got %s", num2charcode(1))
+	}
+	if num2charcode(25) != "z" {
+		t.Errorf("wanted z, got %s", num2charcode(25))
+	}
+	if num2charcode(26) != "aa" {
+		t.Errorf("wanted aa, got %s", num2charcode(26))
+	}
+	if num2charcode(27) != "ab" {
+		t.Errorf("wanted ab, got %s", num2charcode(27))
+	}
+	if num2charcode(51) != "az" {
+		t.Errorf("wanted az, got %s", num2charcode(51))
+	}
+	if num2charcode(52) != "ba" {
+		t.Errorf("wanted ba, got %s", num2charcode(52))
+	}
+	if num2charcode(53) != "bb" {
+		t.Errorf("wanted bb, got %s", num2charcode(53))
+	}
+	if inc("zzzzzzzz") != "aaaaaaaaa" {
+		t.Errorf("wanted aaaaaaaaa, got %s", inc("zzzzzzzz"))
+	}
+}
+
+func TestNum2charcode2(t *testing.T) {
+	for x := 18000; x < 18700; x++ {
+		fmt.Println(num2charcode(x))
+	}
+}
