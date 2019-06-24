f18836e08e44028505cf2da3c59a009252659e3d
Alexander F. Rødseth <rodseth@gmail.com>
2019-06-24 16:01:52 +0200

    Add and update tests

--- xpm_test.go
+++ xpm_test.go
@@ -6,39 +6,39 @@ import (
 )
 
 func TestNum2charcode(t *testing.T) {
-	if num2charcode(0) != "a" {
-		t.Errorf("wanted a, got %s", num2charcode(0))
+	if num2charcode(0, 'a', 'z') != "a" {
+		t.Errorf("wanted a, got %s", num2charcode(0, 'a', 'z'))
 	}
-	if num2charcode(1) != "b" {
-		t.Errorf("wanted b, got %s", num2charcode(1))
+	if num2charcode(1, 'a', 'z') != "b" {
+		t.Errorf("wanted b, got %s", num2charcode(1, 'a', 'z'))
 	}
-	if num2charcode(25) != "z" {
-		t.Errorf("wanted z, got %s", num2charcode(25))
+	if num2charcode(25, 'a', 'z') != "z" {
+		t.Errorf("wanted z, got %s", num2charcode(25, 'a', 'z'))
 	}
-	if num2charcode(26) != "aa" {
-		t.Errorf("wanted aa, got %s", num2charcode(26))
+	if num2charcode(26, 'a', 'z') != "aa" {
+		t.Errorf("wanted aa, got %s", num2charcode(26, 'a', 'z'))
 	}
-	if num2charcode(27) != "ab" {
-		t.Errorf("wanted ab, got %s", num2charcode(27))
+	if num2charcode(27, 'a', 'z') != "ab" {
+		t.Errorf("wanted ab, got %s", num2charcode(27, 'a', 'z'))
 	}
-	if num2charcode(51) != "az" {
-		t.Errorf("wanted az, got %s", num2charcode(51))
+	if num2charcode(51, 'a', 'z') != "az" {
+		t.Errorf("wanted az, got %s", num2charcode(51, 'a', 'z'))
 	}
-	if num2charcode(52) != "ba" {
-		t.Errorf("wanted ba, got %s", num2charcode(52))
+	if num2charcode(52, 'a', 'z') != "ba" {
+		t.Errorf("wanted ba, got %s", num2charcode(52, 'a', 'z'))
 	}
-	if num2charcode(53) != "bb" {
-		t.Errorf("wanted bb, got %s", num2charcode(53))
+	if num2charcode(53, 'a', 'z') != "bb" {
+		t.Errorf("wanted bb, got %s", num2charcode(53, 'a', 'z'))
 	}
-	if inc("zzzzzzzz") != "aaaaaaaaa" {
-		t.Errorf("wanted aaaaaaaaa, got %s", inc("zzzzzzzz"))
+	if inc("zzzzzzzz", 'a', 'z') != "aaaaaaaaa" {
+		t.Errorf("wanted aaaaaaaaa, got %s", inc("zzzzzzzz", 'a', 'z'))
 	}
 }
 
 func TestNum2charcode2(t *testing.T) {
 	for x := 18000; x < 18700; x++ {
-		//fmt.Println(num2charcode(x))
-		num2charcode(x)
+		//fmt.Println(num2charcode(0, 'a', 'z',(x))
+		num2charcode(x, 'a', 'z')
 	}
 }
 
